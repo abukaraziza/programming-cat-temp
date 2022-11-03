97ee62342fb07f367c74d2b5244b236ed9b88f22
abukaraziza <115553393+abukaraziza@users.noreply.github.com>
2022-11-03 20:01:44 +0300

    Add files via upload

---
+++ programming cat temp/programming cat temp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //declare variables
+    float fahrenheit, celcius, kelvin;
+    //prompt useer to enter data
+    cout<<"enter twmprature in fahrenheit";
+    cin>>fahrenheit;
+
+    //calculations
+    celcius =5.0 /9.0 *(farenheit-30)
+    cout<<"the celcius equivalent of"<<fahrenheit << "is =" <<celcius;
+    return 0;
+
+
+}
